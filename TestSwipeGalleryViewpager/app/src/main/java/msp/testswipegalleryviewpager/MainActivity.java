4c219655a4e1c7b4c48ada0316ddbe467dc84cf8
Anita <anita.baier@lmu.de>
2017-06-20 01:41:39 +0200

    gallery test viewpager

---
+++ TestSwipeGalleryViewpager/app/src/main/java/msp/testswipegalleryviewpager/MainActivity.java
@@ -0,0 +1,20 @@
+package msp.testswipegalleryviewpager;
+
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Tutorial: http://manishkpr.webheavens.com/android-viewpager-as-image-slide-gallery-swipe-gallery/
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+        ImageAdapter adapter = new ImageAdapter(this);
+        viewPager.setAdapter(adapter);
+    }
+}
