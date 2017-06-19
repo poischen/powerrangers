bca94769951d59cf3ea4d41fbfc4431b13aee975
Anita <anita.baier@lmu.de>
2017-06-19 04:10:16 +0200

    integration cta buttons in FragmentStart, Upload UserPic in Storage, Speichern des Users in Database

--- Android/app/src/main/java/msp/powerrangers/model/DatabaseAccess.java
+++ Android/app/src/main/java/msp/powerrangers/model/DatabaseAccess.java
@@ -1,14 +1,7 @@
-package msp.powerrangers.database;
+package msp.powerrangers.model;
 
-import android.util.Log;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import static android.content.ContentValues.TAG;
 
 /**
  * This class provides an interface to the Firebase real time database
