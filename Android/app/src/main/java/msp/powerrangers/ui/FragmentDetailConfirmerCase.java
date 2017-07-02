1c4c07a6858b42226a9e30d7f7c9d0b36adef18c
viki <viktorija.velkov@campus.lmu.de>
2017-07-02 11:00:10 +0200

    FragmentDetailConfirmerCase and navigation to it from FragmentConfirmerCases

--- Android/app/src/main/java/msp/powerrangers/ui/FragmentDetailConfirmerCase.java
+++ Android/app/src/main/java/msp/powerrangers/ui/FragmentDetailConfirmerCase.java
@@ -4,106 +4,103 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import msp.powerrangers.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link FragmentDetailConfirmerCase.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link FragmentDetailConfirmerCase#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class FragmentDetailConfirmerCase extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // text Views
+    private TextView textViewConfirmCaseTitle;
+    private TextView textViewConfirmCaseCity;
+    private TextView textViewConfirmCaseCountry;
+    private TextView textViewConfirmCaseScala;
+    private TextView textViewConfirmCaseAreaCoordinates;
+    private TextView textViewConfirmCaseXCoordinate;
+    private TextView textViewConfirmCaseYCoordinate;
+    private TextView textViewConfirmUploadedPictures;
+    private TextView textViewConfirmCaseInformation;
 
-    private OnFragmentInteractionListener mListener;
+    // edit Texts
+    private EditText editTextConfirmCaseTitle;
+    private EditText editTextConfirmCaseCity;
+    private EditText editTextConfirmCaseCountry;
+    private EditText editTextConfirmCaseXCoordinate;
+    private EditText editTextConfirmCaseYCoordinate;
+    private EditText editTextConfirmCaseInformation;
+
+    // radio buttons scale
+    private RadioButton radioButtonConfirmCaseLow;
+    private RadioButton radioButtonConfirmCaseMiddle;
+    private RadioButton radioButtonConfirmCaseHigh;
+
+    // buttons
+    private Button buttonConfirmCaseReport;
+    private ImageView imageViewConfirmUploadedPicture;
 
     public FragmentDetailConfirmerCase() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment FragmentDetailConfirmerCase.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static FragmentDetailConfirmerCase newInstance(String param1, String param2) {
-        FragmentDetailConfirmerCase fragment = new FragmentDetailConfirmerCase();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fr_detail_confirmer_case, container, false);
+        View view = inflater.inflate(R.layout.fr_detail_confirmer_case, container, false);
+
+        // find UI elements
+        // text Views
+        textViewConfirmCaseTitle = (TextView) view.findViewById(R.id.textViewConfirmCaseTitle);
+        textViewConfirmCaseCity = (TextView) view.findViewById(R.id.textViewConfirmCaseCity);
+        textViewConfirmCaseCountry = (TextView) view.findViewById(R.id.textViewConfirmCaseCountry);
+        textViewConfirmCaseScala = (TextView) view.findViewById(R.id.textViewConfirmCaseScala);
+        textViewConfirmCaseAreaCoordinates = (TextView) view.findViewById(R.id.textViewConfirmCaseAreaCoordinates);
+        textViewConfirmCaseXCoordinate = (TextView) view.findViewById(R.id.textViewConfirmCaseXCoordinate);
+        textViewConfirmCaseYCoordinate = (TextView) view.findViewById(R.id.textViewConfirmCaseYCoordinate);
+        textViewConfirmUploadedPictures = (TextView) view.findViewById(R.id.textViewConfirmUploadedPictures);
+        textViewConfirmCaseInformation = (TextView) view.findViewById(R.id.textViewConfirmCaseInformation);
+
+        // edit Texts
+        editTextConfirmCaseTitle = (EditText) view.findViewById(R.id.editTextConfirmCaseTitle);
+        editTextConfirmCaseCity = (EditText) view.findViewById(R.id.editTextConfirmCaseCity);
+        editTextConfirmCaseCountry = (EditText) view.findViewById(R.id.editTextConfirmCaseCountry);
+        editTextConfirmCaseXCoordinate = (EditText) view.findViewById(R.id.editTextConfirmCaseXCoordinate);
+        editTextConfirmCaseYCoordinate = (EditText) view.findViewById(R.id.editTextConfirmCaseYCoordinate);
+        editTextConfirmCaseInformation = (EditText) view.findViewById(R.id.editTextConfirmCaseInformation);
+
+
+        // radio buttons scale
+        radioButtonConfirmCaseLow = (RadioButton) view.findViewById(R.id.radioButtonConfirmCaseLow);
+        radioButtonConfirmCaseMiddle= (RadioButton) view.findViewById(R.id.radioButtonConfirmCaseMiddle);;
+        radioButtonConfirmCaseHigh= (RadioButton) view.findViewById(R.id.radioButtonConfirmCaseHigh);;
+
+        // buttons
+        buttonConfirmCaseReport = (Button) view.findViewById(R.id.buttonConfirmCaseReport);
+        imageViewConfirmUploadedPicture = (ImageView) view.findViewById(R.id.imageViewConfirmUploadedPicture);
+
+
+
+        // Inflate the layout for this fragment
+        return view;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
